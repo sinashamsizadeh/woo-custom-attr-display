9b4753b77a7be831b831a9b19088203820608153
Sina Shamsizadeh <sina_shamsizadeh@hotmail.com>
2022-08-10 13:04:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# woo-custom-attr-display
+ display custom attributes in a custom tabel
--- woo-custom-attr-display.php
+++ woo-custom-attr-display.php
@@ -61,9 +61,13 @@ class Init {
 
         remove_action( 'woocommerce_product_additional_information', 'wc_display_product_attributes', 10 );
         if ( get_option( 'custom_product_attr_display' ) && get_option( 'custom_product_attr_display' ) !== 'hidden'  ) {
-            add_action( get_option( 'custom_product_attr_display' ), [$this, 'woocommerce_custom_attr_display'], 10 );
+            add_action( get_option( 'custom_product_attr_display' ), [$this, 'woocommerce_custom_attr_display'], 22 );
+            add_action( 'wp_head', function() {
+                echo '<style>.woo-custom-attr-display tr td { border: 1px solid #e4e4e4; } h5.custom-product-attr-title { padding-top: 15px; margin-bottom: 10px; }</style>';
+            } );
         }
         add_filter( 'woocommerce_products_general_settings', [$this, 'woocommerce_products_general_settings'] );
+        add_filter( 'woocommerce_display_product_attributes', [$this, 'exclude_from_product_attr'] );
     }
 
     /**
@@ -74,6 +78,10 @@ class Init {
      */
     public function woocommerce_custom_attr_display() {
         global $product;
+
+        if ( ! $product ) {
+            return;
+        }
         
         $product_attributes = array();
 
@@ -134,7 +142,7 @@ class Init {
          * @param array $product_attributes Array of atributes to display; label, value.
          * @param WC_Product $product Showing attributes for this product.
          */
-        $product_attributes = apply_filters( 'woocommerce_display_product_attributes', $product_attributes, $product );
+        $product_attributes = apply_filters( 'custom_woocommerce_display_product_attributes', $product_attributes, $product );
 
         $this->woocommerce_custom_attr_display_output( $product_attributes );
     }
@@ -149,59 +157,120 @@ class Init {
         if ( ! $product_attributes ) {
             return;
         }
-        
+
+        $merge = [];
+        $fragment = explode( ',', get_option( 'custom_product_attr_exclude' ) );
+        $table_title = '';
+       
+        if ( get_option( 'custom_product_attr_title' ) ) {
+            foreach ( $product_attributes as $product_attribute_key => $product_attribute ) :
+                foreach ( $fragment as $key => $value ) {
+                    if ( $value === $product_attributes[$product_attribute_key]['label'] ) {
+                        $table_title = '<h5 class="custom-product-attr-title">' . get_option( 'custom_product_attr_title' ) . '</h5>';
+                    }
+                }
+            endforeach;
+        }
+        if ( $table_title ) {
+            echo wp_kses_post( $table_title );
+        }
         ?>
-        <table class="woocommerce-product-attributes shop_attributes">
+        <table class="woocommerce-product-attributes shop_attributes woo-custom-attr-display">
             <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
                 <?php
                 foreach ( $product_attributes as $product_attribute_key => $product_attribute ) :
-                    if ( strpos( $product_attribute_key, '_pa_') === false  ) {  ?>
-                        <th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
-                        <?php
+                    if ( strpos( $product_attribute_key, '_pa_') === false  ) {
+                        foreach ( $fragment as $key => $value ) {
+                            if ( $value === $product_attributes[$product_attribute_key]['label'] ) {  ?>
+                                <th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
+                                <?php
+                            }
+                        }
                     }
                 endforeach; ?>
             </tr>
             <?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
                 <?php
-                $product_attributes[$product_attribute_key]['value'] = str_replace( ['<p>','</p>'], '', $product_attributes[$product_attribute_key]['value']);
+                $product_attributes[$product_attribute_key]['value'] = str_replace( ['<p>','</p>', "\n"], '', $product_attributes[$product_attribute_key]['value']);
+                $product_attributes[$product_attribute_key]['value'] = str_replace( [', '], ',', $product_attributes[$product_attribute_key]['value']);
                 $product_attributes[$product_attribute_key]['value'] = explode( ',', $product_attributes[$product_attribute_key]['value'] );			
                 ?>
             <?php endforeach; ?>
             
             <?php
-            $merge = [];
             foreach ( $product_attributes as $product_attribute_key => $product_attribute ) :
-                if ( strpos( $product_attribute_key, '_pa_') === false  ) {
-                    $merge[] = $product_attribute['value'];
-                    $size[] = sizeof( $product_attribute['value'] ); 
-                }
-            endforeach;
-            // Fix empty value
-            foreach ( $size as $key => $value ) {
-                if ( max($size) > sizeof( $merge[$key] ) ) {
-                    $merge[$key][] = '';
-                }
-            }
-            for ($i=0; $i <= sizeof($merge); $i++) { 
-                ?>
-                <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
-                    <?php
-                        for ($k=0; $k <= sizeof($merge[$i]); $k++) { 
-                        if ( isset( $merge[$k][$i] ) ) {
-                            ?>
-                            <td class="woocommerce-product-attributes-item__value"> <?php echo wp_kses_post( $merge[$k][$i] ); ?> </td>
-                            <?php
-                        } else {
-                            ?>
-                            <td class="woocommerce-product-attributes-item__value"></td>
-                            <?php              
+                foreach ( $fragment as $key => $value ) {
+                    if ( $value === $product_attributes[$product_attribute_key]['label'] ) {
+                        if ( strpos( $product_attribute_key, '_pa_') === false  ) {
+                            $merge[] = $product_attribute['value'];
+                            $size[] = sizeof( $product_attribute['value'] ); 
                         }
                     }
+                }
+            endforeach;
+
+            // Fix empty value
+            if ( ! empty ( $size ) ) {
+                foreach ( $size as $key => $value ) {
+                    if ( max($size) > sizeof( $merge[$key] ) ) {
+                        $counter = max($size) - sizeof( $merge[$key] );
+                        for ($c=0; $c < $counter; $c++) { 
+                            $merge[$key][] = '';
+                        }
+                    }
+                }
+            }
+
+            if ( ! empty( $merge ) ) {
+
+                for ($i=0; $i < sizeof(array_filter($merge[0])); $i++) { 
                     ?>
-                </tr>
-            <?php } ?>
+                    <tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
+                        <?php
+                            for ($k=0; $k < sizeof($merge); $k++) { 
+                            if ( isset( $merge[$k][$i] ) ) {
+                                ?>
+                                <td class="woocommerce-product-attributes-item__value"> <?php echo wp_kses_post( $merge[$k][$i] ); ?> </td>
+                                <?php
+                            } else {
+                                ?>
+                                <td class="woocommerce-product-attributes-item__value"></td>
+                                <?php              
+                            }
+                        }
+                        ?>
+                    </tr>
+                <?php
+                }
+            }
+            ?>
         </table>
         <?php
+    }
+
+    /**
+     * exclude from product attributes
+     * 
+     * @since 1.0.0
+     * @package CustomDWA
+     */
+    public function exclude_from_product_attr( $product_attributes ) {
+    
+        if ( current_action() == 'woocommerce_display_product_attributes' ) {
+            if ( get_option( 'custom_product_attr_exclude' ) && ! empty( get_option( 'custom_product_attr_exclude' ) ) ) {
+                $fragment = explode( ',', get_option( 'custom_product_attr_exclude' ) );
+                foreach ( $product_attributes as $product_attribute_key => $product_attribute_value ) {
+
+                    foreach ( $fragment as $key => $value ) {
+                        if ( $value === $product_attribute_value['label'] ) {
+                            unset( $product_attributes[$product_attribute_key] );
+                        }
+                    }
+                }
+            }
+        }
+
+        return $product_attributes;
     }
 
     /**
@@ -228,11 +297,31 @@ class Init {
             'type'     => 'select',
             'options'  => array(
                 'hidden'                                => __( 'Hidden', 'woocommerce' ),
-                'woocommerce_single_product_summary'    => __( 'Single product summary', 'woocommerce' ),
+                'woocommerce_single_product_summary'    => __( 'After product summary', 'woocommerce' ),
                 'woocommerce_before_add_to_cart_form'   => __( 'Before add to cart form', 'woocommerce' ),
                 'woocommerce_after_add_to_cart_form'    => __( 'After add to cart form', 'woocommerce' ),
             ),
             'desc_tip' => true,
+        );
+        $settings[] = array(
+            'title'       => __( 'Exclude from product attribute', 'woocommerce' ),
+            'id'          => 'custom_product_attr_exclude',
+            'type'        => 'text',
+            'default'     => '',
+            'class'       => '',
+            'css'         => '',
+            'placeholder' => __( 'Enter custom attribute name', 'woocommerce' ),
+            'desc_tip'    => __( 'seperate names with "," eg: custom attr1,custom attr2', 'woocommerce' ),
+        );
+        $settings[] = array(
+            'title'       => __( 'Table Title', 'woocommerce' ),
+            'id'          => 'custom_product_attr_title',
+            'type'        => 'text',
+            'default'     => '',
+            'class'       => '',
+            'css'         => '',
+            'placeholder' => __( 'Enter Table Title', 'woocommerce' ),
+            'desc_tip'    => __( 'seperate names with "," eg: custom attr1,custom attr2', 'woocommerce' ),
         );
 
         $settings[] = array(
